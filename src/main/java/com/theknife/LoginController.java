af7a80ea4fb16c910c4523536f7cb8deeee0d69b
LoryDePa <depaolilorenzo03@gmail.com>
2025-12-28 14:15:22 +0100

    Login funzionante

--- src/main/java/com/theknife/LoginController.java
+++ src/main/java/com/theknife/LoginController.java
@@ -10,44 +10,24 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
-import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
 public class LoginController {
 
     @FXML private TextField usernameField;
     @FXML private PasswordField passwordField;
-    @FXML private RadioButton clienteRadio;
-    @FXML private RadioButton ristoratoreRadio;
-
-    private ToggleGroup ruoloGroup;
 
     private Stage stage;
-    private Scene scene;
     private Parent root;
-
     private Socket socket;
     private ObjectInputStream in;
     private ObjectOutputStream out;
 
-    // Serve a rendere mutuamente esclusivi i due RadioButton (“Cliente” o “Ristoratore”) 
-    // e garantire che funzioni anche se l’FXML è parzialmente modificato o corrotto.
-    @FXML
-    public void initialize() { 
-        ruoloGroup = new ToggleGroup();
-        if (clienteRadio != null && ristoratoreRadio != null) {
-            clienteRadio.setToggleGroup(ruoloGroup);
-            ristoratoreRadio.setToggleGroup(ruoloGroup);
-        }
-    }
-
     @FXML
     private void onBackClicked(ActionEvent e) throws IOException {
-        //App.setRoot("welcome"); // Eliminare riga
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("welcome.fxml"));
         root = loader.load();
@@ -59,25 +39,86 @@ public class LoginController {
         stage.getScene().setRoot(root);
     }
 
-    // TODO: da completare con la logica di autenticazione lato server
     @FXML
     private void onInviaClicked(ActionEvent e) throws IOException {
-        // SessioneUtente sessione = SessioneUtente.getInstance();
-        // sessione.setUsername(usernameField.getText());
-        // sessione.setPassword(passwordField.getText());
-        // sessione.setRuolo(clienteRadio.isSelected() ? Ruolo.CLIENTE : Ruolo.RISTORATORE);
-        // SessioneUtente.getInstance().stampaDettagli();
+        SessioneUtente sessione = SessioneUtente.getInstance();
+        sessione.setUsername(usernameField.getText());
+        sessione.setPassword(passwordField.getText());
+        SessioneUtente.getInstance().stampaDettagli();
 
-        //App.setRoot("home"); // Eliminare riga
+        // Inserisco dati nel db
+        out.writeObject("login");
+        out.writeObject(sessione.getUsername());
+        out.writeObject(sessione.getPassword());
+        out.flush();
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("home.fxml"));
-        root = loader.load();
+        // Controllo se il login è andato a buon fine e cambio schermata
+        ServerResponse response;
+        try {
+            response = (ServerResponse) in.readObject();
+        } catch (ClassNotFoundException ex) {
+            throw new IOException("Risposta del server non valida", ex);
+        }  
+            
+        if (response.status.equals("OK")) {
+            // Aggiorno la sessione con i dati ricevuti
+            System.out.println(response.getPayload().toString());
+            Utente user = new Utente(
+                response.getPayload().toString().split(";")[0],
+                response.getPayload().toString().split(";")[1],
+                response.getPayload().toString().split(";")[2],
+                response.getPayload().toString().split(";")[3],
+                response.getPayload().toString().split(";")[4],
+                response.getPayload().toString().split(";")[5],
+                response.getPayload().toString().split(";")[6]);
+            sessione.setNome(user.getNome());
+            sessione.setCognome(user.getCognome());
+            sessione.setDataNascita(user.getDataNascita());
+            sessione.setLuogo(user.getDomicilio());
+            sessione.setUsername(user.getUsername());
+            sessione.setPassword(user.getPassword());
+            if(user.getRuolo().equals("ristoratore")) {
+                sessione.setRuolo(Ruolo.RISTORATORE);
+            } else {
+                sessione.setRuolo(Ruolo.CLIENTE);
+            }
+            SessioneUtente.getInstance().stampaDettagli();
 
-        HomeController controller = loader.getController();
-        controller.setConnectionSocket(socket, in, out);
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("home.fxml"));
+                Parent root = loader.load();
 
-        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-        stage.getScene().setRoot(root);
+                HomeController controller = loader.getController();
+                controller.setConnectionSocket(socket, in, out);
+
+                Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+                stage.getScene().setRoot(root);
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Login non riuscito");
+            alert.setHeaderText("Credenziali errate");
+            alert.setContentText("Ritorno alla home...");
+            alert.showAndWait();
+
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("welcome.fxml"));
+                Parent root = loader.load();
+
+                WelcomeController controller = loader.getController();
+                controller.setConnectionSocket(socket, in, out);
+
+                Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+                stage.getScene().setRoot(root);
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
     public void setConnectionSocket(Socket socket, ObjectInputStream in, ObjectOutputStream out){
